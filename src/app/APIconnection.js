e2648a48e9bdb1062651ed3a45e8bc8f26dd4faa
Łukasz Żurawski <zurawskiu@gmail.com>
2020-12-24 20:38:35 +0100

    getiing data from SWAPI

--- src/app/APIconnection.js
+++ src/app/APIconnection.js
@@ -1,23 +1,71 @@
 
-//próba, loguje dane z api
+//dodałem to tylko po to żeby nie zaglądać co chwila do consoli
+function addElement (text, zmienna) {
+   const newDiv = document.createElement("p");
+   const newContent = document.createTextNode(`${text}=====>${zmienna}`);
+   newDiv.appendChild(newContent);
+   const currentDiv = document.getElementById("swquiz-app");
+   document.body.insertBefore(newDiv, currentDiv);
+ }
 
-//people max index = 83/
+
+const el = document.createElement("div");
+// Pytania są generowane w następujący sposób:
+// zostaje pobrany losowy zasób z danego trybu (np people o id 5)
+// zostanie pobrane dla wylosowanego zasobu zdjęcie
+// losowane są 3 odpowiedzi z zapytania do StarWars API. Dla trybu "People" będzie to: https://swapi.co/api/people (jedna brana jest z wcześniej wylosowanego, musi być poprawna)
+
+//TRZEBA NAPISAĆ TESTY!!!!!!!!!!!
+//people max index = 83
+//będziemy losować id z zakresu 1-84 i pobierać właściwość name
 const urlPeopleRequest = 'https://swapi.dev/api/people/';
+
+//PROBLEM: ID STATKOW I POJAZDOW JEST DZIURAWE!!!
+//losowanie strony jest lepsze niz jazda po ID jak przy people bo tutaj mamy często puste ID i zwraca błąd 404, losujemy strony z zakresu 1-5 (5 jest nieosiagalna przy math random wiec zatrzyma się na 4), wchodzimy w .results i odstajemy tablice z obiektami, potem wystarczy wylosować index i pobrać jego name. Mamy losowść.
+
+// Pojawia się jednak problem powtarzalności tak pobranych danych tzn moglibyśmy pobrać 3 odpowiedzi z api z czego dwie lub wszystkie byłyby identyczne i trzeba temu jakoś zaradzić
+const urlVehiclesRequest = 'https://swapi.dev/api/vehicles/?page=';
+const urlStarshipsRequest ='https://swapi.dev/api/starships/?page=';
+
+
 const randomUrlPeopleRequest = (url)=>{
-   const randomizator = Math.floor(Math.random()*84);
-   return `${url}${randomizator}/`
+   const randomizator = Math.floor(Math.random()*(84-1) + 1);
+   return `${url}${randomizator}`
+}
+const randomUrlVehicleStarshipRequest = (url)=>{
+   const randomizator = Math.floor(Math.random()*(5-1) + 1);
+   return `${url}${randomizator}`
 }
 
-//dla pojadow 76
-const urlVehiclesRequest = 'https://swapi.dev/api/vehicles/';
-// 75 dla statków
-const urlStarshipsRequest ='https://swapi.dev/api/starships/';
-
-
-//losowa postac pobrana z api/losowy url/PROBLEM wypada 0
+//teraz jest bez sensu bo powtarzam kod więc trzeba to uprościć
 fetch(randomUrlPeopleRequest(urlPeopleRequest))
 .then(res=> res.json()
 )
 .then(data=>{
-   return console.log(data.name);
+   addElement ('Postać', data.name)
+   return console.log('Postać: ' + data.name);
+})
+
+fetch(randomUrlVehicleStarshipRequest(urlStarshipsRequest))
+.then(res=> {
+   return res.json()
+}
+)
+.then(data=>{
+   const randomIndex = Math.floor(Math.random()*data.results.length);
+   const randomStarshipName = data.results[randomIndex].name;
+   addElement ('Statek kosmiczny',randomStarshipName);
+   return console.log('Statek kosmiczny: ' + randomStarshipName);
+})
+
+fetch(randomUrlVehicleStarshipRequest(urlVehiclesRequest))
+.then(res=> {
+   return res.json()
+}
+)
+.then(data=>{
+   const randomIndex = Math.floor(Math.random()*data.results.length);
+   const randomVehicleName = data.results[randomIndex].name;
+   addElement ('Pojazd',randomVehicleName);
+   return console.log('Pojazd: ' + randomVehicleName);
 })
