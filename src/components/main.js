5f59b88d1ab56bc591bc16434b72a9b31ef20eaf
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-01 19:13:17 +0100

    fix some bugs

--- src/components/main.js
+++ src/components/main.js
@@ -1,77 +1,60 @@
-//do testowania
-const titleTemplate = (question) => {
-  const pInject = `<p>${question}</p>`;
-  return pInject;
-};
-const answearTemplate = () => {
-  const pInject = `<div class="answear1">
-  <p>Luke Skywalker</p>
-</div>
-<div class="answear2">
-  <p>Jar Jar Binks</p>
-</div>
-<div class="answear3">
-  <p>Padme Amidala</p>
-</div>
-<div class="answear4">
-  <p>Darth Vader</p>
-</div>`;
-  return pInject;
-};
-const rankingPeople = {
-  firstPlace: 'Kasia',
-  secondPlace: 'Basia',
-  thirdPlace: 'Ania',
-};
-const rankingTemplate = (name) => {
-  const liInject = `<ul class="ranking-people">
-  <li class="bold">Player</li>
-  <li>${name.firstPlace}</li>
-  <li>${name.secondPlace}</li>
-  <li>${name.thirdPlace}</li>
-</ul>`;
-  return liInject;
-};
-const imgTemplate = () => {
-  const imgInject = `<img src="../../static/assets/img/modes/people/16.jpg" >`;
-  return imgInject;
-};
+//Dynamiczne ładuje cały moduł do wskazanego miejsca po wykonaniu akcji
+// 1. place - selektor mijesca gdzie ma być załadowany moduł
+// 2. listenerSelector - selektor elementu na którym nasłuchujemy
+// 3. fileName - nazwa pliku z którego pobieramy moduł
 
-//Funkcja przyjmuje dwa argumenty:
-// 1. module => callback z templatka html
-// 2. selector => selektor html rodzica - miejsce dodania elementu
-// 3. opt => string reprezentujacy pozycje: beforebegin || domyślnie - afterbegin || beforeend || afterend
-const renderFunction = (module, selector, opt = 'afterbegin') => {
-  let htmlTemplate = `${module}`;
-  const place = document.querySelector(selector);
-  place.insertAdjacentHTML(`${opt}`, htmlTemplate);
-};
+//////PROBLEM
+// 1. Nie działa podanie nazwy pliku, zgłąsza błąd MIME oraz Failed to fetch dynamically imported modul przy odpaleniu przez serwer
 
-// renderFunction(titleTemplate('Co robisz?'), '#swquiz-app');
-// renderFunction(imgTemplate(), '#swquiz-app');
-// renderFunction(answearTemplate(), '#swquiz-app');
-// renderFunction(rankingTemplate(rankingPeople), '.answear4');
-
-//Dynamiczne ładuje cały moduł ze wskazanego miejsca po wykonaniu akcji
-const moduleLoader = (destinationSelector, moduleSelector, fileName) => {
-  // miejsce gdzie moduł ma się ładować
-  const placeToLoad = document.querySelector(destinationSelector);
-  // po wykrytym evencie na moduleActivator np po kliknięciu na People ma się załadować moduł
-  const moduleActivator = document.querySelector(moduleSelector);
-  moduleActivator.addEventListener('click', async (e) => {
-    e.preventDefault();
+const moduleLoader = (place, listenerSelector, fileName) => {
+  const moduleActivator = document.querySelector(listenerSelector);
+  moduleActivator.addEventListener('click', async (event) => {
+    event.preventDefault();
     try {
-      console.log(fileName);
-      // const module = await import(`../app/APIconnection.js`);
-      // 1. Funkcja wykonuje się tylko raz, potem rzuca błędem
-      // 2. Nie działa podanie nazwy pliku, zgłąsza błąd MIME przy odpaleniu przez serwer
-      const module = await import(`../app/${fileName}`);
-      module.append(placeToLoad);
-    } catch {
-      placeToLoad.textContent = 'Błąd';
+      const module = await import(`./${fileName}`);
+      //wywołanie pobranej funkcji z innego pliku
+      // module.renderFunction(place);
+    } catch (e) {
+      console.log(e);
     }
   });
 };
 
-moduleLoader('#swquiz-app', '.btn', 'APIconnection.js');
-moduleLoader('#swquiz-app', '.btn1', 'APIconnection.js');
+// DO TESTOWANIA
+// const rankingTemplate = (name, selector) => {
+//   const liInject = `<ul class="ranking-people">
+//    <li class="bold">Player</li>
+//    <li>${name}</li>
+//  </ul>`;
+//   const place = document.querySelector(selector);
+//   place.innerHTML = liInject;
+// };
+
+//Funkcja przyjmuje dwa argumenty:
+// 1. module => callback z templatka html
+// 2. selector => selektor html rodzica - miejsce dodania elementu
+
+// const renderFn = (module, selector) => {
+//   const htmlTemplate = `${module}`;
+//   const place = document.querySelector(selector);
+//   place.innerHTML = htmlTemplate;
+// };
+
+// export const renderFunction = (selector) => {
+//   const htmlTemplate = document.createElement('img');
+//   htmlTemplate.setAttribute(
+//     'src',
+//     'src="../../static/assets/img/modes/people/16.jpg"',
+//   );
+//   const place = document.querySelector(selector);
+//   place.prepend(htmlTemplate);
+// };
+
+// export const rankingTemplate = (name, selector) => {
+//   const liInject = `<ul class="ranking-people">
+//    <li class="bold">Player</li>
+//    <li>${name}</li>
+//  </ul>`;
+//  const place = document.querySelector(selector)
+//   place.innerHTML = liInject
+// };
