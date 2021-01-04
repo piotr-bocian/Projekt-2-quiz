a425288321485f8644eead167a70e3afb10dea17
double_s <sz.suchodolski@gmail.com>
2021-01-04 23:25:34 +0100

    add css rules and html template to files hall-of-fame.css and .js

--- src/components/hall-of-fame/hall-of-fame.js
+++ src/components/hall-of-fame/hall-of-fame.js
@@ -1,1 +1,27 @@
-console.log('This is Hall-of-fame');
+const loadHallOfFame = () => {
+    const template = document.createElement('template');
+    template.innerHTML = `
+    <div class="ranking">
+            <h2>Mode Ranking</h2>
+            <div>
+                <ul>
+                    <li class="bold">Place</li>
+                    <li>1st</li>
+                    <li>2nd</li>
+                    <li>3rd</li>
+                </ul>
+                <ul class="ranking-people">
+                    <li class="bold">Player</li>
+                    <li>Ania</li>
+                    <li>Mateusz</li>
+                    <li>Leia Organa</li>
+                </ul>
+                <ul class="ranking-score">
+                    <li class="bold">Answered</li>
+                    <li>15/20</li>
+                    <li>14/30</li>
+                    <li>1/23</li>
+                </ul>
+            </div>
+        </div>`
+}
