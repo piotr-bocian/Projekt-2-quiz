bb540af283d8af7fc4752b46af7479ab8782e95d
Klaudia Wojciechowska <k.wojc13@gmail.com>
2021-01-06 12:50:34 +0100

    gameRules folder added

---
+++ src/components/gameRules/gameRules.js
@@ -0,0 +1,25 @@
+function gameRules(text) {
+
+    const divRulesContainer = document.createElement("div");
+    divRulesContainer.className = "rules";
+
+    const divRules = document.createElement("div");
+    divRules.id = "gameRules";
+    divRules.innerHTML = text;
+
+    const icon = document.createElement("img");
+    icon.src = '../../../Projekt-2-quiz/static/assets/ui/graduation-cap-solid.svg';
+    icon.id = 'iconRules';
+
+    const modeRules = document.createElement("div");
+    modeRules.id = "modeRules";
+    const modeText = document.createTextNode("Mode Rules");
+    modeRules.appendChild(icon);
+    modeRules.appendChild(modeText);
+
+    divRulesContainer.appendChild(modeRules);
+    divRulesContainer.appendChild(divRules);
+    return divRulesContainer;
+}
+
+export default gameRules
