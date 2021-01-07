8592a17ddc92f158cdc6781ea9cab5671261d1d2
double_s <sz.suchodolski@gmail.com>
2021-01-07 14:07:49 +0100

    Add class PlayerCPU
    class has also added two methods: askQuestions - this needs to be finished and answerQuestion

--- src/components/playerCPU/playerCPU.js
+++ src/components/playerCPU/playerCPU.js
@@ -1,26 +1,45 @@
 // import 'regenerator-runtime/runtime' //async/await with Parcel
 
-const answerQuestion = (question) => {
-    const cpuRandomPicked = Math.floor(Math.random()*4);
-    const cpuAnswer = question[cpuRandomPicked];
-    return cpuAnswer;
+class PlayerCPU {
+    constructor() {
+        this.noOfQuestions = 0;
+        this.noOfAnswers = 0;
+        this.questionAsked = false;
+        this.questionAnswerd = false;
+        this.answer = '';
+    }
+
+    askQuestion() {
+
+        return question;
+    }
+
+    answerQuestion(question) {
+        const cpuRandomPicked = Math.floor(Math.random()*4);
+        const cpuAnswer = question[cpuRandomPicked];
+        this.questionAnswerd = true;
+        this.noOfAnswers += 1;
+        this.answer = cpuAnswer;
+    }
 }
 
-function playerCPU (question) {
-    const autoPlayer = {
-        answer: "answer",
+const cpu = new PlayerCPU();
 
-    }
+cpu.answerQuestion(['Siara', 'Killer', 'Wąski', 'Ryba']);
+console.log(cpu);
 
-    const playerCPUanswer = answerQuestion(question);
-    autoPlayer.answer = playerCPUanswer;
 
-    return autoPlayer;
-    }
+// Napisać klasę zawierającą to co powinien zawierać obiekt, z funkcjami aksQuestion
+// answerQuestion.
+// Potem napisać funkcję, która w wyniku kliknięcia btn 'playTheGame' tworzy nowy obiekt
+// 'autoPlayer'
 
-const func = playerCPU(['Siara', 'Killer', 'Wąski', 'Ryba']);
-console.log(func);
+
+
+    // const func = playerCPU(['Siara', 'Killer', 'Wąski', 'Ryba']);
+// console.log(func);
 
 // funkcja zadaj pytanie - eventListener('change') na divie wywoła funkcje getAnswers, która
 // pobierze z diva 4 możliwe odp do tablicy i potem zostanie wywołana funkcja answerQestion,
 // losująca odpowiedź i zwracająca ale gdzie? do funkcji sprawdzającej czy odp poprawna?
+
