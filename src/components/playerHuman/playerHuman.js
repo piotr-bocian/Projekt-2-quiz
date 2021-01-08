2817f137708ebb6bb79475f4cfe7aeeafecc4bb8
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-08 11:02:08 +0100

    add class

--- src/components/playerHuman/playerHuman.js
+++ src/components/playerHuman/playerHuman.js
@@ -1,44 +1,27 @@
-const totalQuestions = (selector) => {
-  const allQuestions = [];
-  const questionQuery = document.querySelectorAll(selector);
-  questionQuery.forEach((e) => {
-    allQuestions.push(e.innerHTML);
-  });
-  return allQuestions;
-};
+//do klasy przepisać
 
-const pickedAnswer = (selector) => {
-  let playerAnswer = [];
-  const questionQuery = document.querySelectorAll(selector);
-  questionQuery.forEach((element) => {
-    element.addEventListener(
-      'click',
-      (event) => {
-        playerAnswer.push(event.currentTarget.innerHTML);
-        console.log(playerAnswer);
-      },
-      { once: true },
-    );
-  });
-  return playerAnswer;
-};
-
-//na wyjsciu mam obiekt player
-//allQuestions to tablica, która kumuluje odpowiedzi z divów z pytaniami w formie tablicy, każda wewnętrzna tablica będzie o dlugości 4 elementów, każdy z elementów to zadane pytania;
-//allGames to wszystkie rozegrane gry. Jest równa player.allQuestions.length. Wiemy ile gier rozegrał gracz;
-// pickedAnswer to tablica z odpowiedziami wybranymi przez gracza. Jej długośc będzie się zgadzać z długością tablicy allQuestions.
-function playerGames(allQuestionsClb, pickedAnswersClb) {
-  const questions = allQuestionsClb;
-  const picked = pickedAnswersClb;
-  const player = {
-    allQuestions: [],
-    pickedAnswers: [],
-    allGames: '',
-  };
-  player.allQuestions.push(questions);
-  player.pickedAnswers.push(picked);
-  player.allGames = player.allQuestions.length;
-  return player;
+class PlayerHuman {
+  constructor() {
+    this.pickedAnswer = [];
+    this.correctAnswer = 0;
+  }
+  getAnswer() {
+    let playerAnswer = [];
+    const questionQuery = document.querySelectorAll('.q');
+    questionQuery.forEach((element) => {
+      element.addEventListener(
+        'click',
+        (event) => {
+          playerAnswer.push(event.currentTarget.innerHTM);
+          console.log(playerAnswer);
+        },
+        { once: true },
+      );
+    });
+    return this.pickedAnswer.push(playerAnswer);
+  }
 }
 
-console.log(playerGames(totalQuestions('.q'), pickedAnswer('.q')));
+const player = new PlayerHuman();
+console.log(player);
+player.getAnswer();
