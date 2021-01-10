897fb212dfca3c492b9ac4cfd0b3713179cc0b4e
Jan Eliasz <johnbg@tlen.pl>
2021-01-10 17:54:28 +0100

    Hall of Fame hard-coded

--- src/components/hallOfFame/hallOfFame.js
+++ src/components/hallOfFame/hallOfFame.js
@@ -9,6 +9,31 @@ function showHallOfFame(text){
 
     fameContainer.appendChild(pFame);
 
+    const ranking = document.createElement('div');
+    ranking.classList.add('ranking');
+    ranking.innerHTML = `
+        <ul>
+            <li class="bold">Place</li>
+            <li>1st</li>
+            <li>2nd</li>
+            <li>3rd</li>
+        </ul>
+        <ul class="ranking-people">
+            <li class="bold">Player</li>
+            <li>Ania</li>
+            <li>Mateusz</li>
+            <li>Leia Organa</li>
+        </ul>
+        <ul class="ranking-score">
+            <li class="bold">Answered</li>
+            <li>15/20</li>
+            <li>14/30</li>
+            <li>1/23</li>
+        </ul>
+    `
+
+    fameContainer.appendChild(ranking);
+
     return fameContainer;
 }
 
