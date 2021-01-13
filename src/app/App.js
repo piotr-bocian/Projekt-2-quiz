89254343f909e03f6cdb51bb39a7cff370adc2b0
double_s <sz.suchodolski@gmail.com>
2021-01-13 23:50:10 +0100

    removed startGame function and changed to cpu.restoreDefault - no need for startGame from cpu

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,13 @@
 //aktywuj linijke niżej dla wersji lokalnej
 // import generadeRandomQuestions from '../components/generateQuestion/LocalGenerateQuestion';
 import generadeRandomQuestions from '../components/generateQuestion/generateQuestion';
-import {
-  startGameCPU,
-  cpu,
-  answerAgain,
-} from '../components/playerCPU/playerCPU';
+import { cpu } from '../components/playerCPU/playerCPU';
 export const App = ({ options }) => {
   const playGameButton = document.getElementById('play');
   playGameButton.addEventListener('click', () => {
     console.log('click');
     playGameButton.classList.add('display-none');
+    cpu.restoreDefault();
     generadeRandomQuestions();
-    setTimeout(function () {
-      startGameCPU();
-    }, 300);
   });
 };
