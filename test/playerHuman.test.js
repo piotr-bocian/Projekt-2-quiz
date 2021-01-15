f6137d3f4d7453410cb1ad18a93d6d5f335ca8de
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-15 15:52:01 +0100

    PlayerHuman methods and constructor test work ok

--- test/playerHuman.test.js
+++ test/playerHuman.test.js
@@ -1,7 +1,4 @@
-import {
-  PlayerHuman,
-  playerUpdate,
-} from '../src/components/playerHuman/playerHuman';
+import { PlayerHuman } from '../src/components/playerHuman/playerHuman';
 
 it('should create new player human', () => {
   const player = new PlayerHuman();
@@ -16,11 +13,13 @@ it('should create new player human', () => {
 });
 
 it('should change playerAnswer after using playerChose method', () => {
+  const mockCallback = jest.fn();
+  mockCallback.mockReturnValue('Palpatine');
   const player = new PlayerHuman();
   player.allAnswer = 0;
   player.rightAnswer = 0;
   player.playerAnswer = '';
-  player.playerChose('Palpatine');
+  player.playerChose(mockCallback());
   expect(player).toEqual({
     allAnswer: 0,
     rightAnswer: 0,
@@ -29,11 +28,13 @@ it('should change playerAnswer after using playerChose method', () => {
 });
 
 it('should change value of allAnswer and correctAnswer if answerCounter mothods gets argument = true', () => {
+  const mockCallback = jest.fn();
+  mockCallback.mockReturnValueOnce(true);
   const player = new PlayerHuman();
   player.allAnswer = 0;
   player.rightAnswer = 0;
   player.playerAnswer = 'Yoda';
-  player.answerCounter(true);
+  player.answerCounter(mockCallback());
   expect(player).toEqual({
     allAnswer: 1,
     rightAnswer: 1,
@@ -51,24 +52,5 @@ it('should restore values to default', () => {
     allAnswer: 0,
     rightAnswer: 0,
     playerAnswer: '',
-  });
-});
-
-it('should change value of allAnswer and correctAnswer if answerCounter mothods gets argument = true and set new playerAnswer', () => {
-  document.body.innerHTML = `
-    <div class="questions">
-      <div class="questions_item">Palpatine</div>
-    </div>
-    `;
-  const clickers = document.querySelector('.questions_item');
-  const player = new PlayerHuman();
-  player.allAnswer = 0;
-  player.rightAnswer = 0;
-  player.playerAnswer = '';
-  playerUpdate(clickers, true);
-  expect(player).toEqual({
-    allAnswer: 1,
-    rightAnswer: 1,
-    playerAnswer: 'Palpatine',
   });
 });
