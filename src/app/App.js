584273d121cd3354aad6711b886b970fc45ce2f0
double_s <sz.suchodolski@gmail.com>
2021-01-18 22:11:11 +0100

    fixed blocking mode change after start game

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,11 @@ export const App = ({ options }) => {
   const yellowButton = document.querySelector('.yellow-button');
   // const hallOfGame = document.querySelector('.hall-of-fame-btn');
   playGameButton.addEventListener('click', () => {
+    // block item after start the game
+    const getModeMenu = document.querySelector("mode-change");
+    const ModeItems = getModeMenu.shadowRoot.lastElementChild;
+    ModeItems.setAttribute("style", "pointer-events: none");
+    
     playGameButton.classList.add('display-none');
     yellowButton.style.display = 'none';
     // hallOfGame.classList.add('display-none');
