fb60688aeab1f17867c06a5d6f427bf20dffa352
double_s <sz.suchodolski@gmail.com>
2021-01-18 21:27:02 +0100

    try to fix yellow button display none

--- src/app/App.js
+++ src/app/App.js
@@ -6,11 +6,11 @@ export const App = ({ options }) => {
   mainMenu();
   const playGameButton = document.querySelector('.red-button');
   const yellowButton = document.querySelector('.yellow-button');
-  const hallOfGame = document.querySelector('.hall-of-fame-btn');
+  // const hallOfGame = document.querySelector('.hall-of-fame-btn');
   playGameButton.addEventListener('click', () => {
     playGameButton.classList.add('display-none');
     yellowButton.classList.add('display-none');
-    hallOfGame.classList.add('display-none');
+    // hallOfGame.classList.add('display-none');
     cpu.restoreDefault();
     generadeRandomQuestions(true);
 
