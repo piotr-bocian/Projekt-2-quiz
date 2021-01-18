1708821f7370248c30b642e1e51646aa7694996e
double_s <sz.suchodolski@gmail.com>
2021-01-18 19:02:24 +0100

    test - added .js to import app.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime/runtime'; //async/await with Parcel
-import {App} from "./app/App";
+import {App} from "./app/App.js";
 
 const ONE_SECOND_MILLIS = 1000;
 const SW_API_BASE_URL = process.env.SW_API_BASE_URL || "https://swapi.dev/api";
