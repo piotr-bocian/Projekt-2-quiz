e75da7ec1cb76df7d17fca570397b0c5314b5888
double_s <sz.suchodolski@gmail.com>
2021-01-18 23:04:05 +0100

    media queries for timer changed

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -27,14 +27,16 @@ const timerComponent = () => {
    }
 }
 
+@media only screen and (max-width: 812px) and (orientation: landscape) {
+  .hider, .lighstaberHandler, .lightsaberColor{
+    display: none;
+  }
+
 @media only screen and (min-device-width: 320px) and (max-device-width: 480px){
   .hider, .lighstaberHandler, .lightsaberColor{
     display: none;
   }
-  @media only screen and (max-width: 812px) and (orientation: landscape) {
-    .hider, .lighstaberHandler, .lightsaberColor{
-      display: none;
-    }
+
   </style>
   <div part="loader">
       <div part='lightsaber'>
