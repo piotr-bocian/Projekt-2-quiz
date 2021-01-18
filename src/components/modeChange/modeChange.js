3010f419573052106bf9a0536dc56c82c84172e0
Jan Eliasz <johnbg@tlen.pl>
2021-01-18 17:55:52 +0100

    Changing mode during game fix

--- src/components/modeChange/modeChange.js
+++ src/components/modeChange/modeChange.js
@@ -105,30 +105,30 @@ const modeChange = () => {
                         case 'People':
                           //console.log(gameRules(rulesText.people));
                           gameQuestion(questionText.people);
-                          if(gameRulesContainer != null)
+                          if(gameRulesContainer != null && !(document.querySelector('.questions')))
                             document.body.replaceChild(gameRules(rulesText.people), gameRulesContainer);
                           document.body.replaceChild(gameQuestion(questionText.people), questionContainer);
-                          if(hallOfFameContainer != null)
+                          if(hallOfFameContainer != null && !(document.querySelector('.questions')))
                             document.body.replaceChild(showHallOfFame(fameText.people), hallOfFameContainer);
                           document.body.replaceChild(img(imgSrc.people), imageContainer);
                           break;
                         case 'Vehicles':
                           gameRules(rulesText.vehicles);
                           gameQuestion(questionText.vehivles);
-                          if(gameRulesContainer != null)
+                          if(gameRulesContainer != null && !(document.querySelector('.questions')))
                             document.body.replaceChild(gameRules(rulesText.vehicles), gameRulesContainer);
                           document.body.replaceChild(gameQuestion(questionText.vehicles), questionContainer);
-                          if(hallOfFameContainer != null)
+                          if(hallOfFameContainer != null && !(document.querySelector('.questions')))
                             document.body.replaceChild(showHallOfFame(fameText.vehicles), hallOfFameContainer);
                           document.body.replaceChild(img(imgSrc.vehicles), imageContainer);
                           break;
                         case 'Starships':
                            gameRules(rulesText.starships);
                            gameQuestion(questionText.starships);
-                           if(gameRulesContainer != null)
+                           if(gameRulesContainer != null && !(document.querySelector('.questions')))
                             document.body.replaceChild(gameRules(rulesText.starships), gameRulesContainer);
                            document.body.replaceChild(gameQuestion(questionText.starships), questionContainer);
-                           if(hallOfFameContainer != null)
+                           if(hallOfFameContainer != null && !(document.querySelector('.questions')))
                             document.body.replaceChild(showHallOfFame(fameText.starships), hallOfFameContainer);
                            document.body.replaceChild(img(imgSrc.starships), imageContainer);
                            break; 
