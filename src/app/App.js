276ccd8fea13d002ee059c3c81fbb13eb0ccfdb6
Łukasz Żurawski <zurawskiu@gmail.com>
2021-01-18 15:15:32 +0100

    lightsaber have 70vw and hallOfFame display none

--- src/app/App.js
+++ src/app/App.js
@@ -6,11 +6,13 @@ import { cpu } from '../components/playerCPU/playerCPU';
 export const App = ({ options }) => {
   const playGameButton = document.querySelector('.red-button');
   const yellowButton = document.querySelector('.yellow-button');
+  const hallOfGame = document.querySelector('.hall-of-fame-btn');
   playGameButton.addEventListener('click', () => {
     playGameButton.classList.add('display-none');
     yellowButton.classList.add('display-none');
+    hallOfGame.classList.add('display-none');
     cpu.restoreDefault();
     generadeRandomQuestions(true);
-    
+
   });
 };
